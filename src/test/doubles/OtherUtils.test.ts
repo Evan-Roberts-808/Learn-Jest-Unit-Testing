3b0270cbd45e5e52a8d67e8b859d86b6909009db
Evan <eroberts1998@icloud.com>
2025-01-10 19:01:29 -0500

    set up for next section

---
+++ src/test/doubles/OtherUtils.test.ts
@@ -0,0 +1,11 @@
+describe('OtherUtils test suite', ()=> {
+    it('Calculates Complexity', () => {
+        const someInfo = {
+            length: 5,
+            extraInfo: {
+                field1: 'someInfo',
+                field2: 'anotherInfo'
+            }
+        }
+    })
+})
